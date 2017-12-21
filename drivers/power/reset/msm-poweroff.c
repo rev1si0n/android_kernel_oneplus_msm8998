107ee5d9954e1bc5d329efd46010f865d71b4870
Dan Pasanen <dan.pasanen@gmail.com>
2017-12-20 20:21:50 -0600

    power: don't ever reboot to verity red
    * We get it, shit's broken. We're flashing custom stuff, shit's bound
      to break. Don't pop this annoying screen up, we're not even using
      verity anyway.
    
    Change-Id: Icd77b70ec1df9108a4ba9e7fd8cb9623b35b78db
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -360,10 +360,12 @@ static void msm_restart_prepare(const char *cmd)
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_RTC);
 			__raw_writel(0x77665503, restart_reason);
+#if 0
 		} else if (!strcmp(cmd, "dm-verity device corrupted")) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_DMVERITY_CORRUPTED);
 			__raw_writel(0x77665508, restart_reason);
+#endif
 		} else if (!strcmp(cmd, "dm-verity enforcing")) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_DMVERITY_ENFORCE);
