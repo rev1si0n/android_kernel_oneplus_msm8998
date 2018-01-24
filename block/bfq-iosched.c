fd9925a13ba97bd351bca48ed7aa3f92c3850907
Alexander Martinz <alex@amartinz.at>
2018-01-24 18:54:08 +0100

    block, bfq: fix build breakage
    Caused by latest block changes from 06100.
    struct backing_device_info is not available anymore.
    
    Change-Id: I47ca095ffbad02c3d9158d869d89485ed005435f
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -3995,8 +3995,7 @@ static void bfq_set_next_ioprio_data(struct bfq_queue *bfqq,
 	ioprio_class = IOPRIO_PRIO_CLASS(bic->ioprio);
 	switch (ioprio_class) {
 	default:
-		dev_err(bfqq->bfqd->queue->backing_dev_info.dev,
-			"bfq: bad prio class %d\n", ioprio_class);
+		bfq_log(bfqq->bfqd, "bfq: bad prio class %d\n", ioprio_class);
 	case IOPRIO_CLASS_NONE:
 		/*
 		 * No prio set, inherit CPU scheduling settings.
