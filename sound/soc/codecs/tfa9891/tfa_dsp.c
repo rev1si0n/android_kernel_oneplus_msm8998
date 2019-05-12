d091090f1fb51f090a9a23b0eab8683a46e99946
Jean-Baptiste Théou <jb@essential.com>
2019-05-13 07:46:21 +0900

    ASoC: codecs: tfa9891: add error translation routine
    Clang warns:
    
    ../sound/soc/codecs/tfa98xx/tfa_dsp.c:3155:9: warning: implicit conversion
    from enumeration type 'enum Tfa98xx_Error' to different enumeration type
    'enum tfa_error' [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    ../sound/soc/codecs/tfa98xx/tfa_dsp.c:3215:9: warning: implicit conversion
    from enumeration type 'enum Tfa98xx_Error' to different enumeration type
    'enum tfa_error' [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    ../sound/soc/codecs/tfa98xx/tfa_dsp.c:3267:9: warning: implicit conversion
    from enumeration type 'enum Tfa98xx_Error' to different enumeration type
    'enum tfa_error' [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    
    @neobuddy89: adapted for oneplus-msm8998
    
    Change-Id: I4d1d428f0306bbe6bb25d8beab8a74307aaeadf7
    Signed-off-by: Jean-Baptiste Théou <jb@essential.com>

--- sound/soc/codecs/tfa9891/tfa_dsp.c
+++ sound/soc/codecs/tfa9891/tfa_dsp.c
@@ -2929,6 +2929,24 @@ enum Tfa98xx_Error tfaRunWaitCalibration(Tfa98xx_handle_t handle, int *calibrate
 	return err;
 }
 
+enum tfa_error tfa98xxTotfa(enum Tfa98xx_Error err)
+{
+	switch(err) {
+	case Tfa98xx_Error_Ok:
+		return tfa_error_ok;
+	case Tfa98xx_Error_Device:
+		return tfa_error_device;
+	case Tfa98xx_Error_Bad_Parameter:
+		return tfa_error_bad_param;
+	case Tfa98xx_Error_NoClock:
+		return tfa_error_noclock;
+	case Tfa98xx_Error_StateTimedOut:
+		return tfa_error_timeout;
+	default:
+		return tfa_error_bad_param;
+	}
+}
+
 enum tfa_error tfa_start(int next_profile, int *vstep)
 {
 	enum Tfa98xx_Error err = Tfa98xx_Error_Ok;
@@ -3072,7 +3090,7 @@ error_exit:
 		tfaContClose(dev); /* close all of them */
 	}
 
-	return err;
+	return tfa98xxTotfa(err);
 }
 
 enum tfa_error tfa_stop(void)
@@ -3107,7 +3125,7 @@ enum tfa_error tfa_stop(void)
 error_exit:
 	for( dev=0; dev < devcount; dev++)
 		tfaContClose(dev); /* close all of them */
-	return err;
+	return tfa98xxTotfa(err);
 }
 
 /*
@@ -3159,7 +3177,7 @@ enum tfa_error tfa_reset(void)
 		tfaContClose(dev);
 	}
 
-	return err;
+	return tfa98xxTotfa(err);
 }
 
 /*
